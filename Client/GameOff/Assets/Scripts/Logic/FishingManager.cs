43f9b794b15d314e27192f1b467b66d4bd7faf58
perpsicat9831 <759584856@qq.com>
2022-11-19 18:59:35 +0800

    打表，部分表格功能逻辑

---
+++ Client/GameOff/Assets/Scripts/Logic/FishingManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+namespace Logic
+{
+    public class FishingManager : BaseManager<FishingManager>
+    {
+        /// <summary>
+        /// 钓鱼阶段ID列表
+        /// </summary>
+        private List<int> listStageId = new List<int>();
+        /// <summary>
+        /// 钓鱼按住阶段时长列表
+        /// </summary>
+        private List<int> listStageTime = new List<int>();
+
+        public enum EEvents
+        {
+            onEventTrigger,
+        }
+        public EventEmitter<EEvents> eventEmitter = new EventEmitter<EEvents>();
+
+
+        #region lifeCycle
+        public override void OnInit()
+        {
+            InitData();
+        }
+        public override void OnDestroy()
+        {
+
+        }
+        public void OnUpData()
+        {
+            //需要继承接口 IManagerUpdateModule 
+        }
+        #endregion
+
+        #region func
+        private void InitData()
+        {
+            listStageId.Clear();
+            listStageTime.Clear();
+            var listData = CSVManager.CSVData.TbFishingStage.DataList;
+            for (int i = 0; i < listData.Count; i++)
+            {
+                var curData = listData[i];
+                listStageId.Add(curData.Id);
+                listStageTime.Add(curData.FishingTime);
+            }
+        }
+
+        /// <summary>
+        /// 根据钓鱼时长，获取itemId
+        /// </summary>
+        /// <returns></returns>
+        public int GetFishingRewardItemId(float time)
+        {
+            float deltaTime = 0;
+            int stageId = 0;
+            for (int i = 0; i < listStageTime.Count; i++)
+            {
+                deltaTime += listStageTime[i];
+                if (time > deltaTime)
+                {
+                    stageId = listStageId[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var stageData = CSVManager.CSVData.TbFishingStage.GetOrDefault(stageId);
+            if (stageData != null)
+            {
+
+            }
+            return 0;
+        }
+        #endregion
+
+        #region event
+        #endregion
+
+    }
+}
---
+++ Client/GameOff/Assets/Scripts/Logic/PlayerManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+namespace Logic
+{
+    public class PlayerManager : BaseManager<PlayerManager>
+    {
+
+        public enum EEvents
+        {
+            onEventTrigger,
+        }
+        public EventEmitter<EEvents> eventEmitter = new EventEmitter<EEvents>();
+
+
+        #region lifeCycle
+        public override void OnInit()
+        {
+
+        }
+        public override void OnDestroy()
+        {
+
+        }
+        public void OnUpData()
+        {
+            //需要继承接口 IManagerUpdateModule 
+        }
+        #endregion
+
+        #region func
+        /// <summary>
+        /// 获取玩家速度倍率
+        /// </summary>
+        public float GetPlayerSpeedRate()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.PlayerSpeedMul;
+            }
+            return 1;
+        }
+        /// <summary>
+        /// 获取玩家冲撞CD
+        /// </summary>
+        public float GetPlayerDashCD()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.DashCd;
+            }
+            return 1;
+        }
+        /// <summary>
+        /// 获取玩家重生时间
+        /// </summary>
+        public float GetPlayerRebornTime()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.RebornTime;
+            }
+            return 1;
+        }
+        /// <summary>
+        /// 获取玩家重生后保护时间
+        /// </summary>
+        public float GetPlayerRebornProtectTime()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.RebornProtect;
+            }
+            return 1;
+        }
+        /// <summary>
+        /// 获取玩家体积倍数
+        /// </summary>
+        public float GetPlayerVolumeRate()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.PlayerVolMul;
+            }
+            return 1;
+        }
+        /// <summary>
+        /// 获取玩家击退距离
+        /// </summary>
+        public float GetPlayerDashDic()
+        {
+            var data = CSVManager.CSVData.TbPlayerProp.GetOrDefault(1);
+            if (data != null)
+            {
+                return data.PlayerHitbackMul;
+            }
+            return 1;
+        }
+        #endregion
+
+        #region event
+        #endregion
+
+    }
+}
