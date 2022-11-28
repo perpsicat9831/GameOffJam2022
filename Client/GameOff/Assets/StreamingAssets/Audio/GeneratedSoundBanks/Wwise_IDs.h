5b5abaab4cfd5be8166d6173126959895d569355
lijiacheng01 <lijiacheng01@bilibili.com>
2022-11-28 16:00:35 +0800

    大厅UI音效资源

--- Client/GameOff/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Client/GameOff/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,17 +14,62 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID TEST_MUSIC = 379521629U;
+        static const AkUniqueID UI_BUTTON_CLICK_NEG = 1020395736U;
+        static const AkUniqueID UI_BUTTON_CLICK_POS = 1729881340U;
+        static const AkUniqueID UI_BUTTON_ENABLE = 4004560084U;
+        static const AkUniqueID UI_BUTTON_LEAVE = 4078704050U;
+        static const AkUniqueID UI_BUTTON_READY = 186347966U;
+        static const AkUniqueID UI_BUTTON_START = 1937610011U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BGM
+        {
+            static const AkUniqueID GROUP = 412724365U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID VICTORY = 2716678721U;
+            } // namespace STATE
+        } // namespace BGM
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME_MASTER = 3695994288U;
+        static const AkUniqueID VOLUME_MUSIC = 3891337659U;
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+        static const AkUniqueID VOLUME_SOUND = 4030497411U;
+        static const AkUniqueID VOLUME_UI = 4212040332U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID INTERFACE = 1088520938U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NPC = 662417162U;
         static const AkUniqueID PLAYER = 1069431850U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NPC = 662417162U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SOUND = 623086306U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
